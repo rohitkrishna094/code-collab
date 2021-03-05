0a4d2e40e485ba4b96973ecae5c1bf018bf7de63
rohitkrishna094 <rohitkrishna094@gmail.com>
2021-03-04 22:48:20 -0500

    test commit

--- frontend/src/components/CodeEditor/CodeEditor.tsx
+++ frontend/src/components/CodeEditor/CodeEditor.tsx
@@ -99,7 +99,7 @@ const initialTerminalState = [
   },
 ];
 
-const CodeEditor = ({ userName }: CodeEditorProp) => {
+const CodeEditor = ({ userName }: CodeEditorProps) => {
   const [mode, setMode] = useState('java');
   const [langId, setLangId] = useState(getDefaultLangId());
   const [codeValue, setCodeValue] = useState(getDefaultMode(langId + ''));
